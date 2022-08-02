ebdc93955c0ca287efc15fa45839115d0089e271
Dorako <dirako@gmail.com>
2022-08-02 11:21:11 +0200

    make token select show skills

---
+++ src/view/Skill.svelte
@@ -0,0 +1,147 @@
+<script>
+   import Actions from "./Actions.svelte";
+   import ProfButton from "./ProfButton.svelte";
+   import UpButton from "./UpButton.svelte";
+   let actionGroupSelected = "available";
+
+   function handleClick() {
+      alert("Level up!");
+   }
+</script>
+
+<div class="wrapper">
+   <div class="card">
+      <div class="titleGroup">
+         <div class="title">Deception</div>
+         <div class="right">
+            <UpButton on:click={handleClick} />
+            <ProfButton />
+         </div>
+      </div>
+      <hr />
+      <div class="description">
+         {_token.}
+         You can trick and mislead others using disguises, lies, and other forms of subterfuge.
+      </div>
+   </div>
+   <div class="actions">
+      <div class="group">
+         <div
+            class={actionGroupSelected === "available" ? "active" : ""}
+            on:click={() => (actionGroupSelected = "available")}
+         >
+            Available (4)
+         </div>
+         <div
+            class={actionGroupSelected === "unavailable" ? "active" : ""}
+            on:click={() => (actionGroupSelected = "unavailable")}
+         >
+            Unavailable (1)
+         </div>
+         <div class={actionGroupSelected === "feat" ? "active" : ""} on:click={() => (actionGroupSelected = "feat")}>
+            Feat (3)
+         </div>
+      </div>
+      <Actions />
+   </div>
+</div>
+
+<style lang="scss">
+   .card {
+      background: #f8f4f1;
+      border: 1px solid rgba(0, 0, 0, 0.28);
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.18);
+      border-radius: 5px;
+      padding: 10px;
+
+      .titleGroup {
+         display: flex;
+         justify-content: space-between;
+         gap: 10px;
+         //  font-size: larger;
+
+         .title {
+            color: #5e0000;
+            font-variant-caps: small-caps;
+            font-size: 100px;
+            font-family: "Modesto Condensed";
+         }
+      }
+      hr {
+         border-top: 1px solid #e5e1de;
+      }
+
+      .text {
+         color: #1c1c1c;
+         line-height: 1.2;
+         mask-image: linear-gradient(180deg, #000 80%, transparent);
+      }
+
+      .buttons {
+         display: flex;
+         gap: 5px;
+         justify-content: space-between;
+         gap: 10px;
+         .left {
+            display: flex;
+            gap: 5px;
+         }
+      }
+   }
+   .wrapper {
+      display: flex;
+      flex-direction: column;
+      // width:300px;
+      // flex: 1 0 450px;
+      // width: 300px;
+      // height: 100%;
+      // padding: 10px;
+      // background: linear-gradient(90deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,0.2) 5%, transparent 100%);;
+
+      .titleGroup {
+         display: flex;
+         justify-content: space-between;
+         gap: 10px;
+         .title {
+            color: #5e0000;
+            font-variant-caps: small-caps;
+            font-weight: bold;
+            font-size: xx-large;
+         }
+         .right {
+            display: flex;
+            gap: 5px;
+         }
+      }
+      hr {
+         width: 100%;
+         border-top: 1px solid #d1cbc5;
+      }
+
+      .description {
+         font-style: italic;
+         margin-bottom: 10px;
+         font-family: Roboto;
+      }
+
+      .actions {
+         margin-top: 10px;
+         display: flex;
+         flex-direction: column;
+         gap: 10px;
+         .group {
+            cursor: pointer;
+            display: flex;
+            justify-content: space-around;
+            text-decoration: underline;
+            .active {
+               text-decoration: underline;
+
+               text-decoration-color: black;
+               color: #5e0000;
+               font-weight: bold;
+            }
+         }
+      }
+   }
+</style>
